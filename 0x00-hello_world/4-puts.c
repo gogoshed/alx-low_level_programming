28256480cca99f0ed4262a48612db4aaf686fa63
gogoshed <gogoshedrack@gmail.com>
2022-09-24 13:53:22 -0700

    like puzzle programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <stdio.h>                                                                
-/**                                                                        
- *
- * main - printing in c language                                        
- *
- * Return: always 0 (successful)                                                 *
- **/                                                                                    
-
-int main(void)                                                                                                                                      
-{                                                                                  puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-
+#include <sdio.h>
+/**
+ * Description: main - Prins out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
